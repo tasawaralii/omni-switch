3320b4cfb8718eff928b29a7669f59544f27a150
Tasawar Ali <speedodubbers@gmail.com>
2026-06-11 12:49:42 +0500

    Added backend to use .env

--- api-server/database.py
+++ api-server/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost/omni_switch_db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
